6ef417a38aa667c211a8c1d4c47a18c45416b9eb
lynn <es.lynn@outlook.sg>
2023-02-08 18:50:24 +0800

    fix no ipv6 available

--- modules/frontend/src/services/identme.ts
+++ modules/frontend/src/services/identme.ts
@@ -4,11 +4,17 @@ async function fetchIpAddress(): Promise<{
   ipv4: string
   ipv6?: string
 }> {
-  const ipv4 = (await axios.get('https://4.ident.me/')).data
-  const ipv4or6 = (await axios.get('https://6.ident.me/')).data
+  let ipv4: string
+  let ipv6: string | undefined
+  ipv4 = (await axios.get('https://4.ident.me/')).data
+  try {
+    ipv6 = (await axios.get('https://6.ident.me/')).data
+  } catch (error) {
+    console.warn('no IPv6 address available')
+  }
   return {
     ipv4: ipv4,
-    ipv6: ipv4 !== ipv4or6 ? ipv4or6 : undefined
+    ipv6: ipv6
   }
 }
 
