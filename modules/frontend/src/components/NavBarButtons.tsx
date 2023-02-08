acba5123002a7fd1c713737b150432ef172f08b6
trung hieu dao <dao_trung_hieu@tech.gov.sg>
2023-02-09 02:50:01 +0800

    clickable nav bar button

--- modules/frontend/src/components/NavBarButtons.tsx
+++ modules/frontend/src/components/NavBarButtons.tsx
@@ -1,14 +1,40 @@
-import { Avatar, Box } from 'native-base'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Avatar, Box, Menu, Pressable } from 'native-base'
+import React from 'react'
+
+import { Cfg } from '../config/config'
+import { Modal } from '../modal/ModalController'
 
 // TODO get user initials
-// TODO on click avatar
 // TODO admin [...] button on projects screen
 export const NavBarButtons = (): React.ReactNode => {
+  const { logout } = useAuth0()
+
   return (
     <Box px={4}>
-      <Avatar size="sm" bg="cyan.600">
-        AA
-      </Avatar>
+      <Menu
+        trigger={triggerProps => (
+          <Pressable {...triggerProps}>
+            <Avatar size="sm" bg="cyan.600">
+              AA
+            </Avatar>
+          </Pressable>
+        )}
+      >
+        <Menu.Item
+          onPress={() =>
+            Modal.confirm2({
+              title: 'Logout',
+              type: 'danger',
+              body: 'Are you sure you wish to logout?',
+              onConfirm: () => logout({ logoutParams: { returnTo: Cfg.APP_DOMAIN } }),
+              confirmText: 'Logout'
+            })
+          }
+        >
+          Log out
+        </Menu.Item>
+      </Menu>
     </Box>
   )
 }
