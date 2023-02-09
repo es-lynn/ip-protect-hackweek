427af1d88e4ae0a7c9eeeffad304f97263cfbcbf
Dao Trung Hieu <trung.hieu.dao@gmail.com>
2023-02-10 04:30:05 +0800

    Feature/link qr (#43)
    * link qr code screen
    
    * tweaking back button to match jaslyns routing
    
    ---------
    
    Co-authored-by: trung hieu dao <dao_trung_hieu@tech.gov.sg>

--- modules/frontend/src/components/NavBar.tsx
+++ modules/frontend/src/components/NavBar.tsx
@@ -2,19 +2,22 @@ import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import { Box, ChevronLeftIcon, HStack, IconButton, Text } from 'native-base'
 import React from 'react'
 
+import { nav } from '../router/nav'
+import { path } from '../router/route'
+
 export const NavBar = (props: NativeStackHeaderProps) => {
   const titleSize = 'xl'
 
   return (
     <Box bg="primary.700" pt={4}>
       <HStack maxWidth={480} w="100%" alignSelf="center" alignItems="center" minHeight={10}>
-        {props.options.headerBackVisible !== false && props.navigation.canGoBack() && (
+        {props.options.headerBackVisible && (
           <IconButton
             icon={<ChevronLeftIcon color={props.options.headerTintColor} />}
             rounded="full"
             position="absolute"
             left={0}
-            onPress={() => props.navigation.goBack()}
+            onPress={() => nav.navigate(path.home.index)}
           />
         )}
         <Text
--- modules/frontend/src/pages/project/_components/User/InvitationCreateDialog.tsx
+++ modules/frontend/src/pages/project/_components/User/InvitationCreateDialog.tsx
@@ -1,5 +1,18 @@
-import { Button, FormControl, Input, Modal, Select } from 'native-base'
-import React from 'react'
+import * as Clipboard from 'expo-clipboard'
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  HStack,
+  Input,
+  Modal,
+  Select,
+  Text,
+  VStack
+} from 'native-base'
+import React, { useState } from 'react'
+import SvgQRCode from 'react-native-qrcode-svg'
 
 import { AsyncButton } from '../../../../components/AsyncButton'
 import { useFormState } from '../../../../hooks/useFormState'
@@ -24,56 +37,90 @@ export const InvitationCreateDialog = ({
     email: ''
   })
 
+  const [url, setUrl] = useState<string>()
+  const [copied, setCopied] = useState(false)
+
   return (
     <Modal.Content>
       <Modal.CloseButton />
-      <Modal.Header>User</Modal.Header>
+      <Modal.Header>Create link</Modal.Header>
       <Modal.Body>
-        <FormControl>
+        {url ? (
+          <VStack space={6}>
+            <Text>Scan this QR code</Text>
+            <Box alignSelf="center" borderColor="muted.200" borderWidth={2} rounded="xl" p={4}>
+              <SvgQRCode value={url} size={240} />
+            </Box>
+            <Divider />
+            <Text>Or share this invitation link.</Text>
+            <HStack>
+              <Input type="text" flexGrow={1} value={url} editable={false} />
+              <Button
+                flexShrink={0}
+                variant="outline"
+                onPress={() => {
+                  Clipboard.setStringAsync(url)
+                  setCopied(true)
+                }}
+              >
+                {copied ? 'Copied!' : 'Copy'}
+              </Button>
+            </HStack>
+          </VStack>
+        ) : (
           <FormControl>
-            <FormControl.Label>Email</FormControl.Label>
-            <Input
-              value={form.email}
-              onChangeText={async text => {
-                setForm('email', text)
-              }}
-            />
+            <FormControl>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input
+                value={form.email}
+                onChangeText={async text => {
+                  setForm('email', text)
+                }}
+                placeholder="(optional) restrict link"
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormControl.Label>Expires</FormControl.Label>
+              <Select
+                minWidth="200"
+                _selectedItem={{ bg: 'teal.600' }}
+                mt="1"
+                selectedValue={form.duration.toString()}
+                onValueChange={value => {
+                  setForm('duration', Number.parseInt(value))
+                }}
+              >
+                <Select.Item key={'1_hour'} label={'1 hour'} value={'3600'} />
+                <Select.Item key={'1_day'} label={'1 day'} value={'86400'} />
+                <Select.Item key={'1_week'} label={'1 week'} value={'604800'} />
+              </Select>
+            </FormControl>
           </FormControl>
-          <FormControl isRequired>
-            <FormControl.Label>Expires</FormControl.Label>
-            <Select
-              minWidth="200"
-              _selectedItem={{ bg: 'teal.600' }}
-              mt="1"
-              selectedValue={form.duration.toString()}
-              onValueChange={value => {
-                setForm('duration', Number.parseInt(value))
-              }}
-            >
-              <Select.Item key={'1_hour'} label={'1 hour'} value={'3600'} />
-              <Select.Item key={'1_day'} label={'1 day'} value={'86400'} />
-              <Select.Item key={'1_week'} label={'1 week'} value={'604800'} />
-            </Select>
-          </FormControl>
-        </FormControl>
+        )}
       </Modal.Body>
       <Modal.Footer>
         <Button.Group space={2}>
-          <Button variant="ghost" colorScheme="blueGray" onPress={() => modal.cancel()}>
-            Cancel
-          </Button>
-          <AsyncButton
-            onPress={async () => {
-              const url = await createInviteLink(
-                projectFriendlyId,
-                form.duration,
-                form.email
-              ).catch(throwToastAPIError)
-              modal.ok(url)
-            }}
-          >
-            Add
-          </AsyncButton>
+          {url ? (
+            <Button onPress={() => modal.cancel()}>Close</Button>
+          ) : (
+            <>
+              <Button variant="ghost" colorScheme="blueGray" onPress={() => modal.cancel()}>
+                Cancel
+              </Button>
+              <AsyncButton
+                onPress={async () => {
+                  const url = await createInviteLink(
+                    projectFriendlyId,
+                    form.duration,
+                    form.email
+                  ).catch(throwToastAPIError)
+                  setUrl(url)
+                }}
+              >
+                Add
+              </AsyncButton>
+            </>
+          )}
         </Button.Group>
       </Modal.Footer>
     </Modal.Content>
