d3f589411af30152b7e398d7c202c989f672e707
lynn <es.lynn@outlook.sg>
2023-02-10 01:34:33 +0800

    home button

--- modules/frontend/src/router/Router.tsx
+++ modules/frontend/src/router/Router.tsx
@@ -1,7 +1,10 @@
+import { Ionicons } from '@expo/vector-icons'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Avatar, Icon, Pressable } from 'native-base'
 import React from 'react'
 
+import { colors } from '../components/Avatar'
 import { NavBarButtons } from '../components/NavBarButtons'
 import { DebugLoginPage } from '../pages/auth/login/DebugLoginPage'
 import { LoginPage } from '../pages/auth/login/LoginPage'
@@ -10,7 +13,10 @@ import { DebugPage } from '../pages/debug/DebugPage'
 import { HomePage } from '../pages/home/HomePage'
 import { InvitePage } from '../pages/invite/InvitePage'
 import { ProjectPage } from '../pages/project/ProjectPage'
-import { navigationRef } from './nav'
+import { sp } from '../styles/space'
+import { format } from '../utils/format'
+import { generateRandomNumber } from '../utils/random.util'
+import { nav, navigationRef } from './nav'
 import { path } from './route'
 
 const Stack = createNativeStackNavigator()
@@ -46,6 +52,17 @@ export const Router = (): React.ReactElement => {
     >
       <Stack.Navigator
         screenOptions={{
+          headerLeft: () => (
+            <Pressable onPress={() => nav.navigate(path.home.index)}>
+              <Icon
+                as={Ionicons}
+                name="home"
+                color="white"
+                size={7}
+                style={{ marginLeft: sp._16 }}
+              />
+            </Pressable>
+          ),
           headerRight: NavBarButtons,
           headerStyle: { backgroundColor: '#0E7490' },
           headerTintColor: '#FFFFFF',
