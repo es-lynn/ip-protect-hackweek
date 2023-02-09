86b6de396ea5209aa54524e7ca2f37bf6efdfc51
trung hieu dao <dao_trung_hieu@tech.gov.sg>
2023-02-09 23:37:35 +0800

    login and invite pages

--- modules/frontend/src/router/Router.tsx
+++ modules/frontend/src/router/Router.tsx
@@ -1,8 +1,9 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 
 import { NavBarButtons } from '../components/NavBarButtons'
+import { DebugLoginPage } from '../pages/auth/login/DebugLoginPage'
 import { LoginPage } from '../pages/auth/login/LoginPage'
 import { RedirectPage } from '../pages/auth/redirect/RedirectPage'
 import { DebugPage } from '../pages/debug/DebugPage'
@@ -14,10 +15,19 @@ import { path } from './route'
 
 const Stack = createNativeStackNavigator()
 
+const Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'white'
+  }
+}
+
 export const Router = (): React.ReactElement => {
   return (
     <NavigationContainer
       ref={navigationRef}
+      theme={Theme}
       linking={{
         prefixes: [],
         enabled: true,
@@ -25,6 +35,7 @@ export const Router = (): React.ReactElement => {
           screens: {
             [path.debug.index]: 'debug',
             [path.auth.login]: 'auth/login',
+            [path.auth.debuglogin]: 'auth/debuglogin',
             [path.auth.redirect]: 'auth/redirect',
             [path.home.index]: 'home',
             [path.invite.index]: 'invite',
@@ -38,18 +49,43 @@ export const Router = (): React.ReactElement => {
           headerRight: NavBarButtons,
           headerStyle: { backgroundColor: '#0E7490' },
           headerTintColor: '#FFFFFF',
-          headerShadowVisible: false
+          headerShadowVisible: false,
+          headerTitleStyle: { fontSize: 24 }
+          // header: props => {
+          //   return (
+          //     <Box
+          //       tintColor={props.options.headerTintColor}
+          //       bg={props.options.headerStyle?.['backgroundColor']}
+          //     >
+          //       {props.options.back}
+          //       Hi
+          //     </Box>
+          //   )
+          // }
         }}
       >
         <Stack.Screen
           name={path.auth.login}
           component={LoginPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name={path.auth.debuglogin}
+          component={DebugLoginPage}
           options={{ headerRight: undefined }}
         />
         <Stack.Screen name={path.auth.redirect} component={RedirectPage} />
-        <Stack.Screen name={path.invite.index} component={InvitePage} />
+        <Stack.Screen
+          name={path.invite.index}
+          component={InvitePage}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name={path.debug.index} component={DebugPage} />
-        <Stack.Screen name={path.home.index} component={HomePage} />
+        <Stack.Screen
+          name={path.home.index}
+          component={HomePage}
+          options={{ contentStyle: { backgroundColor: '#F5F5F5' } }}
+        />
         <Stack.Screen name={'project/:projectFriendlyId'} component={ProjectPage} />
       </Stack.Navigator>
     </NavigationContainer>
