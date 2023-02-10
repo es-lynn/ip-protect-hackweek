84b514fdf862a294e0d4a5dca6eb61668af831b0
Dao Trung Hieu <trung.hieu.dao@gmail.com>
2023-02-10 10:04:15 +0800

    stop the tabs from scrolling (#47)
    Co-authored-by: trung hieu dao <dao_trung_hieu@tech.gov.sg>

--- modules/frontend/src/pages/project/ProjectPage.tsx
+++ modules/frontend/src/pages/project/ProjectPage.tsx
@@ -51,9 +51,46 @@ export const _ProjectPage: React.FC = () => {
   }, [nav, isAdmin, access])
 
   return (
-    <ScrollView>
-      <SafeAreaView style={{ backgroundColor: 'white' }}>
-        <Box bg="primary.700" w="full" h="50px" position="absolute" />
+    <Box height="100%">
+      <ScrollView>
+        <SafeAreaView style={{ backgroundColor: 'white' }}>
+          <Box maxWidth={480} w="100%" alignSelf="center" pt={50}>
+            {selectedTab === 'ip-address' && (
+              <IpAddressView
+                ipAddresses={ipAddresses}
+                whitelistedV4={whitelistedV4}
+                whitelistedV6={whitelistedV6}
+                projectFriendlyId={projectFriendlyId}
+                deleteIpAddress={deleteIpAddress}
+                addIpAddress={addIpAddress}
+              />
+            )}
+
+            {selectedTab === 'websites' && (
+              <WebpageView
+                webpages={webpages}
+                projectFriendlyId={projectFriendlyId}
+                addWebpage={addWebpage}
+                deleteWebpage={deleteWebpage}
+                isAdmin={isAdmin}
+              />
+            )}
+
+            {selectedTab === 'users' && (
+              <UserView
+                users={users}
+                projectFriendlyId={projectFriendlyId}
+                editProjectUserRole={editProjectUserRole}
+                addProjectUser={addProjectUser}
+                removeProjectUser={removeProjectUser}
+                createInviteLink={createInviteLink}
+                isAdmin={isAdmin}
+              />
+            )}
+          </Box>
+        </SafeAreaView>
+      </ScrollView>
+      <Box position="absolute" w="100%" bg="primary.700">
         <Tabs
           labels={[
             { label: 'IP addresses', value: 'ip-address', badge: ipAddresses?.length },
@@ -63,42 +100,8 @@ export const _ProjectPage: React.FC = () => {
           selected={selectedTab}
           onPressTab={index => setSelectedTab(index)}
         />
-        <Box maxWidth={480} w="100%" alignSelf="center">
-          {selectedTab === 'ip-address' && (
-            <IpAddressView
-              ipAddresses={ipAddresses}
-              whitelistedV4={whitelistedV4}
-              whitelistedV6={whitelistedV6}
-              projectFriendlyId={projectFriendlyId}
-              deleteIpAddress={deleteIpAddress}
-              addIpAddress={addIpAddress}
-            />
-          )}
-
-          {selectedTab === 'websites' && (
-            <WebpageView
-              webpages={webpages}
-              projectFriendlyId={projectFriendlyId}
-              addWebpage={addWebpage}
-              deleteWebpage={deleteWebpage}
-              isAdmin={isAdmin}
-            />
-          )}
-
-          {selectedTab === 'users' && (
-            <UserView
-              users={users}
-              projectFriendlyId={projectFriendlyId}
-              editProjectUserRole={editProjectUserRole}
-              addProjectUser={addProjectUser}
-              removeProjectUser={removeProjectUser}
-              createInviteLink={createInviteLink}
-              isAdmin={isAdmin}
-            />
-          )}
-        </Box>
-      </SafeAreaView>
-    </ScrollView>
+      </Box>
+    </Box>
   )
 }
 
